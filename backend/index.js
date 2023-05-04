53b2906ee591514d9396d140772af212e7a68788
Mariano Labatut <marianolabatut@gmail.com>
2023-05-03 22:44:03 -0300

    Rest Client para facilitar mis peticiones HTTP.

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ app.use(cors({ origin: true }));
 
 app.post("/authenticate", async (req, res) => {
     const { username } = req.body;
-    return res.json({ username: username, secret: "sha256..." });
+    return res.json({ username: username, password: "teodora1961" });
 });
 
-app.listen(3000);
+app.listen(3001);
